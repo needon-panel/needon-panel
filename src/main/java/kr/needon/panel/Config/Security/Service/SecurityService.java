1368e1f7c7a7ebe6317d3b05df4d5f9cbb972d16
Hyun Woo <sw@needon.kr>
2020-11-03 10:39:00 +0900

    Initial commit

--- src/main/java/kr/needon/panel/Config/Security/Service/SecurityService.java
+++
@@ -1,8 +0,0 @@
-package kr.needon.panel.Config.Security.Service;
-
-public interface SecurityService {
-
-    String findLoggedInUsername();
-    void autoLogin(String username, String password);
-
-}
--- src/main/java/kr/needon/panel/Module/User/Repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.needon.panel.Module.User.Repository;
-
-import kr.needon.panel.Module.User.Model.RoleBean;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<RoleBean, Long> {
-}
