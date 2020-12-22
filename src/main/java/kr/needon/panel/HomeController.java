40a685df8ec59a2d62a6c6dfcbc084cd36aa8709
Hyun Woo <sw@needon.kr>
2020-12-22 23:43:13 +0900

    소스 기록

--- src/main/java/kr/needon/panel/HomeController.java
+++ src/main/java/kr/needon/panel/HomeController.java
@@ -1,15 +1,29 @@
 package kr.needon.panel;
 
+import com.spotify.docker.client.DefaultDockerClient;
+import com.spotify.docker.client.DockerClient;
+import com.spotify.docker.client.exceptions.DockerCertificateException;
+import com.spotify.docker.client.exceptions.DockerException;
+import com.spotify.docker.client.messages.Container;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String Home() {
+    public @ResponseBody String Home() throws DockerException, InterruptedException, DockerCertificateException {
         System.out.println("test");
-        return "index";
+
+        final DockerClient docker = DefaultDockerClient.fromEnv().build();
+
+        final List<Container> containers = docker.listContainers(DockerClient.ListContainersParam.allContainers());
+
+
+        return containers.toString();
     }
 
 }
--- src/test/java/kr/needon/panel/PanelApplicationTests.java
+++ src/test/java/kr/needon/panel/PanelApplicationTests.java
@@ -1,13 +1,31 @@
 package kr.needon.panel;
 
+import com.spotify.docker.client.DefaultDockerClient;
+import com.spotify.docker.client.DockerClient;
+import com.spotify.docker.client.exceptions.DockerCertificateException;
+import com.spotify.docker.client.exceptions.DockerException;
+import com.spotify.docker.client.messages.Container;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class PanelApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testDockerList() throws DockerCertificateException, DockerException, InterruptedException {
+        final DockerClient docker = DefaultDockerClient.fromEnv().build();
+
+        final List<Container> containers = docker.listContainers(DockerClient.ListContainersParam.allContainers());
+
+        System.out.println("containers --> " + containers.toString());
+
+
     }
 
 }
